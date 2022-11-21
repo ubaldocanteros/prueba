d24453c7eb00e430114a7410199300b96a1102bc
uby <ubaldodavidcanteros@gmail.com>
2022-11-21 19:55:47 -0300

    prueba

---
+++ js/variables.js
@@ -0,0 +1,10 @@
+let genero = "pop";
+let musica = "general";
+let año = "2000";
+let genero_y_musica = genero + " " + musica;
+
+genero = "Rock";
+musica = "General";
+
+console.log(genero, musica, año);
+alert(genero_y_musica);
